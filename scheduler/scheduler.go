3785d8f8f3822d7aeef98354cd3227e88fdb9cdf
xinyao.cheng <xinyao.cheng@zerone.com.cn>
2021-07-10 08:04:32 +0800

    调度

---
+++ scheduler/scheduler.go
@@ -0,0 +1,69 @@
+package scheduler
+
+
+import (
+	"log"
+	"encoding/json"
+	"github.com/chengcxy/gotools/backends"
+    "github.com/chengcxy/gotools/configor"
+	"github.com/chengcxy/gotools/roboter"
+) 
+
+
+type Scheduler struct {
+	config *configor.Config
+	taskId string
+	robot roboter.Roboter
+	taskInfo *TaskInfo
+
+}
+
+func NewScheduler(config *configor.Config,taskId string) *Scheduler{
+	sd := &Scheduler{
+		config:config,
+		taskId:taskId,
+		robot:roboter.GetRoboter(config),
+	}
+	sd.GetTaskInfo()
+	return sd
+}
+
+func (sd *Scheduler)GetTaskInfo(){
+	McInterface, ok := sd.config.Get("taskmeta")
+	if !ok {
+		panic("taskmeta key not in json_file")
+	}
+	conf := McInterface.(map[string]interface{})
+	taskDbConfig := conf["conn"]
+	QueryTaskSql := conf["query_task"].(string)
+	log.Println("QueryTaskSql is ",QueryTaskSql)
+	mc := backends.NewMysqlConfig(taskDbConfig)
+	mysql := backends.NewMysqlClient(mc)
+	defer mysql.Close()
+	datas,_, err := mysql.Query(QueryTaskSql,sd.taskId)
+	if err != nil{
+		log.Println("query taskmeta err",err)
+		panic("query taskmeta err")
+	}
+	if len(datas) != 1{
+		panic("taskId not in taskTable")
+	}
+	meta := datas[0]//map[string]string
+	metaBytes,err := json.Marshal(meta)
+	if err != nil{
+		log.Println("taskmeta get,but trans bytes error",err)
+		panic("taskmeta get,but trans bytes error")
+	}
+	err = json.Unmarshal(metaBytes,&sd.taskInfo)
+	if err != nil{
+		log.Println("taskmeta Unmarshal for taskInfo error ",err)
+		panic("taskmeta Unmarshal for taskInfo error ")
+	}
+	
+}
+
+
+func (sd *Scheduler)Run(){
+	log.Printf("sd.taskInfo.Params is \n %s",sd.taskInfo.Params)
+	
+}
---
+++ scheduler/types.go
@@ -0,0 +1,20 @@
+package scheduler
+
+
+
+
+type TaskInfo  struct {
+	Id             string `json:"id"`              // 统计任务id
+	ExecutedEngine string `json:"executed_engine"` // 统计流向来源
+	ToDbType       string `json:"to_db_type"`      // 写入数据源类型
+	ToDb           string `json:"to_db"`           // 写入数据库
+	ToTable        string `json:"to_table"`        // 写入数据表
+	StaticRule     string `json:"static_rule"`     // 统计规则
+	Params         string `json:"params"`          // 增量规则
+	OnlineStatus   string `json:"online_status"`   // 统计状态0统计1不统计
+	WriteMode      string `json:"write_mode"`      // 写入模式replace/insert/append/overwrite
+	TaskDesc       string `json:"task_desc"`       // 统计描述
+	IsTruncate     string `json:"is_truncate"`     // 是否truncate表(0-truncate)
+	Owner          string `json:"owner"`           // 取数人
+	TaskStatus     string `json:"task_status"`     // 任务状态
+}
