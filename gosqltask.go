0e1566604a5da84f4fb8c68464c2551fbe18c292
xinyao.cheng <xinyao.cheng@zerone.com.cn>
2021-07-10 08:20:18 +0800

    添加命令行支持

--- gosqltask.go
+++ gosqltask.go
@@ -3,18 +3,28 @@ package main
 
 import (
 
+	"flag"
+	"log"
 	"github.com/chengcxy/gotools/configor"
 	"github.com/chengcxy/gosqltask/scheduler"
 )
 
+var ConfigPath string
+var Env string
+var TaskId string
+
+func init() {
+	flag.StringVar(&ConfigPath, "c", "./config/", "配置文件目录")
+	flag.StringVar(&Env, "e", "test", "运行的环境-json文件前缀 dev/test/prod/local")
+	flag.StringVar(&TaskId, "id", "1",  "任务id")
+	flag.Parse()
+	log.Println(ConfigPath, Env, TaskId)
+}
 
 
 func main(){
-	taskId := "1"
-	configPath := "/Users/chengxinyao/go/src/github.com/chengcxy/gosqltask/config"
-	Env := "dev"
-	config := configor.NewConfig(configPath,Env)
-	sd := scheduler.NewScheduler(config,taskId)
+	config := configor.NewConfig(ConfigPath, Env)
+	sd := scheduler.NewScheduler(config,TaskId)
 	sd.Run()
 }
 
