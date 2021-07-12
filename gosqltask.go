d74ff81fc785b3aecec0a89337e81cc0dbaf61f1
chengxinyao <xinyao.cheng@zerone.com.cn>
2021-07-12 11:43:22 +0800

    变更表结果 新增调度器属性

--- gosqltask.go
+++ gosqltask.go
@@ -12,10 +12,13 @@ var ConfigPath string
 var Env string
 var TaskId string
 
+var Cmd string
+
 func init() {
 	flag.StringVar(&ConfigPath, "c", "./config/", "配置文件目录")
 	flag.StringVar(&Env, "e", "test", "运行的环境-json文件前缀 dev/test/prod/local")
 	flag.StringVar(&TaskId, "id", "1",  "任务id")
+	flag.StringVar(&Cmd, "cmd", " cat ",  "")
 	flag.Parse()
 	log.Printf("ConfigPath: %s ,Env: %s ,TaskId: %s ",ConfigPath, Env,TaskId)
 }
@@ -24,7 +27,7 @@ func init() {
 func main(){
 	config := configor.NewConfig(ConfigPath, Env)
 	sd := scheduler.NewScheduler(config,TaskId)
-	sd.Run()
+	sd.Run(Cmd)
 }
 
 
