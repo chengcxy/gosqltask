ecf67761d3fc2a6870592890dce651060b0ae8de
chengxinyao <xinyao.cheng@zerone.com.cn>
2021-07-13 11:38:51 +0800

    readme

--- data/sql_task.sql
+++ data/sql_task.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.10-log)
 # Database: z_pe
-# Generation Time: 2021-07-13 02:36:40 +0000
+# Generation Time: 2021-07-13 03:37:00 +0000
 # ************************************************************
 
 
@@ -29,11 +29,12 @@ DROP TABLE IF EXISTS `sql_tasks`;
 CREATE TABLE `sql_tasks` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '统计任务id',
   `from_app` varchar(50) DEFAULT NULL COMMENT '统计来源业务系统',
+  `from_db_type` varchar(50) DEFAULT NULL COMMENT '读取的数据源类型',
   `from_db` varchar(20) DEFAULT NULL COMMENT '来自数据库',
   `static_rule` text COMMENT '统计规则',
   `params` text COMMENT '增量规则',
-  `to_db_type` varchar(20) DEFAULT NULL COMMENT '写入数据源类型',
   `to_app` varchar(50) DEFAULT NULL COMMENT '写入的业务系统',
+  `to_db_type` varchar(20) DEFAULT NULL COMMENT '写入数据源类型',
   `to_db` varchar(20) DEFAULT NULL COMMENT '写入数据库',
   `to_table` varchar(255) DEFAULT NULL COMMENT '写入数据表',
   `online_status` int(11) DEFAULT NULL COMMENT '统计状态0统计1不统计',
@@ -50,9 +51,9 @@ CREATE TABLE `sql_tasks` (
 LOCK TABLES `sql_tasks` WRITE;
 /*!40000 ALTER TABLE `sql_tasks` DISABLE KEYS */;
 
-INSERT INTO `sql_tasks` (`id`, `from_app`, `from_db`, `static_rule`, `params`, `to_db_type`, `to_app`, `to_db`, `to_table`, `online_status`, `write_mode`, `task_desc`, `is_truncate`, `owner`, `create_time`, `task_status`, `update_time`)
+INSERT INTO `sql_tasks` (`id`, `from_app`, `from_db_type`, `from_db`, `static_rule`, `params`, `to_app`, `to_db_type`, `to_db`, `to_table`, `online_status`, `write_mode`, `task_desc`, `is_truncate`, `owner`, `create_time`, `task_status`, `update_time`)
 VALUES
-	(1,'local_dw','z_pe','select $start as start,$end as end,num\nfrom (\n	select count(1) as num\n	from $table \n        where $pk>$start and $pk<=$end \n) as a\n','{\"split\":{\"table\":\"test.userinfo\",\"pk\":\"id\",\"worker_num\":20,\"read_batch\":500,\"write_batch\":300}}','mysql','local_dw','z_pe','temp_static',0,'insert','统计',0,'18811788263','2021-03-30 16:13:34','executed_success','2021-07-13 10:36:29');
+	(1,'local_dw','mysql','z_pe','select $start as start,$end as end,num\nfrom (\n	select count(1) as num\n	from $table \n        where $pk>$start and $pk<=$end \n) as a\n','{\"split\":{\"table\":\"test.userinfo\",\"pk\":\"id\",\"worker_num\":20,\"read_batch\":500,\"write_batch\":300}}','local_dw','mysql','z_pe','temp_static',0,'insert','统计',0,'18811788263','2021-03-30 16:13:34','executed_success','2021-07-13 11:18:33');
 
 /*!40000 ALTER TABLE `sql_tasks` ENABLE KEYS */;
 UNLOCK TABLES;
