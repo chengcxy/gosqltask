162c781741ffac9897066313e617c9b077e10c10
chengxinyao <xinyao.cheng@zerone.com.cn>
2021-07-14 17:47:43 +0800

    add readme printlog

--- gosqltask.go
+++ gosqltask.go
@@ -17,7 +17,7 @@ var Debug bool
 
 func init() {
 	flag.StringVar(&ConfigPath, "c", "./config/", "配置文件目录")
-	flag.StringVar(&Env, "e", "test", "运行的环境-json文件前缀 dev/test/prod/local")
+	flag.StringVar(&Env, "e", "dev", "运行的环境-json文件前缀 dev/test/prod/local")
 	flag.StringVar(&TaskId, "id", "1",  "任务id")
 	flag.BoolVar(&Debug, "d", false, "debug执行的sql")
 	flag.BoolVar(&Debug, "debug", false, "debug执行的sql")
@@ -32,7 +32,7 @@ func main(){
 	sd := scheduler.NewScheduler(config,TaskId)
 	sd.Run(Debug)
 	endTime := time.Now()
-	log.Println(endTime.Sub(startTime))
+	log.Println("costs ",endTime.Sub(startTime))
 }
 
 
